8b6007769c9aa6e567c358263f72dd1c973ff8bd
Danilo Pianini <danilo.pianini@unibo.it>
2024-02-19 15:07:09 +0100

    ci: add the OOP machinery

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+
 plugins {
     java
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.danilopianini.gradle-java-qa") version "1.34.0"
+
+    id("org.danilopianini.unibo-oop-gradle-plugin") version "1.0.4-dev03-d2a3b5e"
 }
+
 
 repositories {
     mavenCentral()
@@ -28,3 +32,5 @@ tasks.test {
 application {
     mainClass = "it.unibo.fnafretro.Program"
 }
+
+ java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
